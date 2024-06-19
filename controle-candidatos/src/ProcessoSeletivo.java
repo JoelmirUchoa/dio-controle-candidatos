ff217bdf0584e71e88febf5609ed45195cc1f777
Joelmir Uchoa <joelmiruchoa@gmail.com>
2024-06-18 21:07:45 -0300

    Imprimindo uma lista com os candidatos selecionados

--- controle-candidatos/src/ProcessoSeletivo.java
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -2,8 +2,27 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        selecaoCanditatos();
+        imprimirSelecionados();
     }
+
+    //Imprimindo uma lista com os candidatos selecionados
+    static void imprimirSelecionados() {
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+
+        for(int indice=0; indice < candidatos.length; indice++) {
+        System.out.println("O candidato de n°" + (indice+1) + " é o " + candidatos [indice]);
+        }
+
+        System.out.println("Forma abrevida de interação for each");
+
+        for (String candidato: candidatos) {
+        System.out.println("O candidato selecionado foi" + candidato);
+        }
+    }
+
+    //Selecionando candidatos
     static void selecaoCanditatos() {
         String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
 
@@ -26,6 +45,7 @@ public class ProcessoSeletivo {
             return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
     
+    //Analisando candidatos
     static  void analisarCanditato(double salarioPretendido) {
         double salarioBase = 2000.0;
         if(salarioBase > salarioPretendido) {
